dfe890cc83695e35e46abf34252680deab74b938
slaman-farcy <salmanfarcy253@gmail.com>
2024-08-14 16:47:30 +0600

    Another method is to export named

--- createmodule/Phone.js
+++ createmodule/Phone.js
@@ -1,13 +1,13 @@
 
-//For example, 2 function is declared
-let sumData = (a, b) => {
+//* For example, 2 function is declared
+// Another method is to export named
+export let sumData = (a, b) => {
     return a + b;
 }
 
-const minData = (d, f) =>{
+// Another method is to export named
+export const minData = (d, f) =>{
     return d - f;
 }
 
 
-//Exported by named 
-export {sumData, minData};
