0068a10aa74e9c7c07623ee7eeeb7ac2113d8184
slaman-farcy <salmanfarcy253@gmail.com>
2024-08-14 16:35:44 +0600

    By default js ES6 a how to export and import

---
+++ createmodule/Phone.js
@@ -0,0 +1,9 @@
+
+//For example, a function is declared
+let sumData = (a, b) => {
+    return a + b;
+}
+
+
+//Exported by default
+export default sumData;
---
+++ createmodule/index.js
@@ -0,0 +1,6 @@
+//Imported by default
+import sumData from "./Phone.js";
+
+//and function is used
+console.log(sumData(3,7));
+
