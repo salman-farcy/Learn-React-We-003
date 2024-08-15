5aaa18f0a6113a55d67b15a5bc65baa88ee21d4b
slaman-farcy <salmanfarcy253@gmail.com>
2024-08-15 12:46:51 +0600

    What is React Component and how is it used?

--- seson-001/src/App.js
+++ seson-001/src/App.js
@@ -1,32 +1,19 @@
 import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 
 function App() {
-  let name = "salman faycy";
-  let l = [10, 20, 30, 40];
-  let obj = {
-    "name": "salmna",
-    "age": 34,
-    "class": "10"
-  }
-  let status = true;
+  
   return (
-    <div className="main">
-      <h1>{name}</h1>
-      <div >
-        {l.map((v) => {
-          return <div >{v}</div>;
-        })}
-      </div>
-      {
-      status 
-      ?
-      <h1 style={{color:"red", backgroundColor:"yellow"}}>salman farcy</h1>
-      :
-      ''
-      }
-      
-    </div>
+   <div>
+    <Header />
+    <h1>salman farcy</h1>
+    <Footer />
+   </div>
   );
 }
 
 export default App;
+
+
+
---
+++ seson-001/src/Components/Header.jsx
@@ -0,0 +1,23 @@
+
+
+
+
+const Header = () => {
+  return (
+    <>
+      <h1>Welcome to Header Section</h1>
+      <Card />
+      
+    </>
+  )
+}
+
+export default Header;
+
+function Card(){
+    return(
+        <div>
+            <p>Card Component</p>
+        </div>
+    )
+}
