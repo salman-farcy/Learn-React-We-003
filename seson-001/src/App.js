ab0383ab8742816ed595f18665f7cb3e1cb838ca
slaman-farcy <salmanfarcy253@gmail.com>
2024-08-15 12:10:53 +0600

    How to write a jsx in reactHow to write a jsx in react

--- seson-001/src/App.js
+++ seson-001/src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let name = "salman faycy";
+  let l = [10, 20, 30, 40];
+  let obj = {
+    "name": "salmna",
+    "age": 34,
+    "class": "10"
+  }
+  let status = true;
   return (
-    <div className="App">
-      <h1>Salman farcy</h1>
+    <div className="main">
+      <h1>{name}</h1>
+      <div >
+        {l.map((v) => {
+          return <div >{v}</div>;
+        })}
+      </div>
+      {
+      status 
+      ?
+      <h1 style={{color:"red", backgroundColor:"yellow"}}>salman farcy</h1>
+      :
+      ''
+      }
+      
     </div>
   );
 }
