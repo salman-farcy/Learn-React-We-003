bed3525ccc4aa6f00b6f4f43ad15e649bea94ca8
slaman-farcy <salmanfarcy253@gmail.com>
2024-08-20 08:34:51 +0600

    Implemented react image add method

--- season-002-tailwind/src/App.jsx
+++ season-002-tailwind/src/App.jsx
@@ -1,6 +1,7 @@
 
-import './App.css'
-import Header from './Components/header/Header'
+import './App.css';
+import Header from './Components/header/Header';
+import weblogo from "./image/images.jpeg"
 
 function App() {
   
@@ -8,9 +9,11 @@ function App() {
   return (
     <div>
       <Header />
+      <img src={weblogo} height={200} width={200} alt="this is my photo" />
       <h1 className='bg-red-700 text-4xl text-green-700 font-bold'>welcome Bangladesh in my cuntery</h1>
     </div>
   )
 }
 
-export default App
+export default App;
+ 
