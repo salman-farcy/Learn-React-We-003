70047d5db97ac12d95e599efdcc53157c05f3334
slaman-farcy <salmanfarcy253@gmail.com>
2024-08-21 09:14:14 +0600

    module style component create and use system

--- state-managment/src/App.jsx
+++ state-managment/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import btnModuleCss from "./component/Btn-module/Button.module.css"
 import Header from "./component/header/Header";
 
 function App() {
@@ -14,6 +15,10 @@ function App() {
       <button onClick={() => setStatus(!status)} className="bg-blue-400 py-3 px-7">{status ? <p>Header Hide</p> : <p>Header show</p>}</button>
 
       <h1 className="bg-green-600">salman farcy the greate</h1>
+
+      <div>
+          <button className={btnModuleCss.error}>Error</button>
+      </div>
     </>
   );
 }
