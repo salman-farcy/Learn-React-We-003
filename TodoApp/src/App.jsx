96601aeeb7ae4dbd1bb9ab0dcf6ff83802074c5d
slaman-farcy <salmanfarcy253@gmail.com>
2024-08-26 07:19:13 +0600

    update

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -40,7 +40,7 @@ function App() {
           placeholder="Write yours ToDo list"
         />
         <button className="basis-3/12 bg-red-100 py-2 px-5  rounded-md border-2 text-gray-400">
-          Save
+          save
         </button>
       </form>
 
