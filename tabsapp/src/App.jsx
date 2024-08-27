d65efb3a52a8c45b55e54f7d090c1310b2f875e1
slaman-farcy <salmanfarcy253@gmail.com>
2024-08-27 07:41:05 +0600

    #feature/Tab section created

--- tabsapp/src/App.jsx
+++ tabsapp/src/App.jsx
@@ -1,13 +1,44 @@
-
+import { useState } from "react";
+import { tabDatas } from "./Data/tabData";
 
 function App() {
+  let [activeTabs, setActiveTabs] = useState(0);
+  let [activeContante, setActiveComtante] = useState(tabDatas[0]);
 
-
+  let changeData = (index) => {
+    setActiveTabs(index)
+    setActiveComtante(tabDatas[index])
+  };
   return (
-    <>
-      <h1 className="text-4xl">salman farcy</h1>
-    </>
-  )
+    <div>
+      <div className="w-3/4 mx-auto bg-slate-400">
+        <h1 className="text-4xl text-center">
+          Law Prep Vision Mission and Values
+        </h1>
+
+        <ul className="flex gap-2">
+          {tabDatas.map((tabsItems, index) => {
+            return (
+              <li key={index}>
+                <button
+                  onMouseOver={() => changeData(index)}
+                //  onClick={() => changeData(index)}
+                  className={activeTabs == index ? "bg-red-300 px-4 py-2 cursor-pointer" : "bg-gray-200 px-4 py-2 cursor-pointer"}
+                >
+                  {tabsItems.title}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+        {activeContante !== undefined ? (
+          <p>{activeContante.description}</p>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ tabsapp/src/Data/tabData.jsx
@@ -0,0 +1,21 @@
+
+
+export let tabDatas = [
+   {
+      "title":"Vision",
+      "description" : "React lets you build user interfaces out of individual pieces called components. Create your own React components like Thumbnail, LikeButton, and Video. Then combine them into entire screens, pages, and apps."
+   },
+   {
+      "title":"Missoon",
+      "description" : "next.js lets you build user interfaces out of individual pieces called components. Create your own React components like Thumbnail, LikeButton, and Video. Then combine them into entire screens, pages, and apps."
+   },
+   {
+      "title":"Values",
+      "description" : "many lets you build user interfaces out of individual pieces called components. Create your own React components like Thumbnail, LikeButton, and Video. Then combine them into entire screens, pages, and apps."
+   },
+   {
+      "title":"Faq",
+      "description" : "what is your quesen lets you build user interfaces out of individual pieces called components. Create your own React components like Thumbnail, LikeButton, and Video. Then combine them into entire screens, pages, and apps."
+   },
+   
+]
