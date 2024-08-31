9fb3bb89d552e5677b0772fe3f78b22e5288cc38
slaman-farcy <salmanfarcy253@gmail.com>
2024-08-31 10:52:03 +0600

    The form data has been created

--- from/src/App.jsx
+++ from/src/App.jsx
@@ -1,37 +1,68 @@
 import { useState } from "react"
-import { FaEyeSlash, FaRegEye } from "react-icons/fa";
+// import { FaEyeSlash, FaRegEye } from "react-icons/fa";
 
 
 
 function App() {
-let [showPassword, setShowPassword] = useState(false)
-let [uname, setUname] = useState('');
-let [password, setPassword] = useState('');
+  const [formData, setFormData] = useState({
+    uname: "",
+    uemail: "",
+    uphone: "",
+    umessage: "",
+    index: "",
+  });
+  const [userData, setUserData] = useState([]);
 
-const handelSubmite = (e) =>{
-  e.preventDefault()
-  console.log(uname, password)
-}
+  let getValue = (e) => {
+    let oldData = {...formData}
+    let inputName = e.target.name;
+    let inputValue = e.target.value;
+   oldData[inputName] = inputValue
+   setFormData(oldData)
+  };
+
+  let handleSubmit = (e) => {
+    let currentUserFormdata = {
+      uname: formData.uname,
+      uemail: formData.uemail,
+      uphone: formData.uphone,
+      umessage: formData.umessage,
+    }
+    let oldData = [...userData, currentUserFormdata]
+    setUserData(oldData)
+    e.preventDefault();
+  }
 
   return (
     <div>
       <div className="">
         <div className="">
-          <form onSubmit={handelSubmite}>
+          <form onSubmit={handleSubmit}>
              <div className="">
-              <label>UserName</label>
-              <input type="text" onChange={(e) => setUname(e.target.value)} value={uname} />
-             </div>
-             <div className="">
-              <label>Password</label>
-              <input type={`${showPassword ? "text" : "password"}`}  onChange={(e) => setPassword(e.target.value)} value={password} />
-              <button onClick={() => setShowPassword(!showPassword)}>{showPassword ? <FaRegEye /> : <FaEyeSlash />}</button>
-              
-              
+              <label>Name</label>
+              <input onChange={getValue} type="text" name="uname" value={formData.uname} className="border-2 border-black" />
              </div>
 
              <div className="">
-              <button className="bg-red-200 py-2 px-5 rounded-md">Login</button>
+              <label>Email</label>
+              <input onChange={getValue} type="text" name="uemail" value={formData.uemail}  className="border-2 border-black" />
+             </div>
+
+             <div className="">
+              <label>Phone</label>
+              <input onChange={getValue} type="text" name="uphone" value={formData.uphone} className="border-2 border-black" />
+             </div>
+
+            <div className="">
+              <label>Message</label>
+              <textarea onChange={getValue} name="umessage" value={formData.umessage} id="" rows="3" className="border-2 border-black"></textarea>
+            </div>
+             <div className="">
+              <button className="bg-red-200 py-2 px-5 rounded-md">
+                {
+                  formData.index == "" ? "Submit" : "Update"
+                }
+              </button>
              </div>
           </form>
         </div>
