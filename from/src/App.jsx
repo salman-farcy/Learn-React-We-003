080eb2a5794a46c3969a06fe401ddf302510cc79
slaman-farcy <salmanfarcy253@gmail.com>
2024-09-01 13:37:41 +0600

    #update-code/Added functionality to show from data

--- from/src/App.jsx
+++ from/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react"
+import { useState } from "react";
 // import { FaEyeSlash, FaRegEye } from "react-icons/fa";
-
-
 
 function App() {
   const [formData, setFormData] = useState({
@@ -13,62 +11,138 @@ function App() {
   });
   const [userData, setUserData] = useState([]);
 
+  // The value from the input box is obtained through this function
   let getValue = (e) => {
-    let oldData = {...formData}
+    let oldData = { ...formData };
     let inputName = e.target.name;
     let inputValue = e.target.value;
-   oldData[inputName] = inputValue
-   setFormData(oldData)
+    oldData[inputName] = inputValue;
+    setFormData(oldData);
+    
   };
 
   let handleSubmit = (e) => {
+    e.preventDefault();
     let currentUserFormdata = {
       uname: formData.uname,
       uemail: formData.uemail,
       uphone: formData.uphone,
       umessage: formData.umessage,
-    }
-    let oldData = [...userData, currentUserFormdata]
-    setUserData(oldData)
-    e.preventDefault();
-  }
+    };
+    let oldAndNewData = [...userData, currentUserFormdata];
+    setUserData(oldAndNewData);
+    setFormData(
+      {
+        uname: "",
+        uemail: "",
+        uphone: "",
+        umessage: "",
+        index: "",
+      }
+    )
+  };
 
   return (
     <div>
-      <div className="">
+      <div className="w-[1200px] mx-auto flex justify-between">
         <div className="">
           <form onSubmit={handleSubmit}>
-             <div className="">
+            <div className="">
               <label>Name</label>
-              <input onChange={getValue} type="text" name="uname" value={formData.uname} className="border-2 border-black" />
-             </div>
+              <input
+                onChange={getValue}
+                type="text"
+                name="uname"
+                value={formData.uname}
+                required
+                className="border-2 border-black"
+              />
+            </div>
 
-             <div className="">
+            <div className="">
               <label>Email</label>
-              <input onChange={getValue} type="text" name="uemail" value={formData.uemail}  className="border-2 border-black" />
-             </div>
+              <input
+                required
+                onChange={getValue}
+                type="text"
+                name="uemail"
+                value={formData.uemail}
+                className="border-2 border-black"
+              />
+            </div>
 
-             <div className="">
+            <div className="">
               <label>Phone</label>
-              <input onChange={getValue} type="text" name="uphone" value={formData.uphone} className="border-2 border-black" />
-             </div>
+              <input
+                required
+                onChange={getValue}
+                type="text"
+                name="uphone"
+                value={formData.uphone}
+                className="border-2 border-black"
+              />
+            </div>
 
             <div className="">
               <label>Message</label>
-              <textarea onChange={getValue} name="umessage" value={formData.umessage} id="" rows="3" className="border-2 border-black"></textarea>
+              <textarea
+                required
+                onChange={getValue}
+                name="umessage"
+                value={formData.umessage}
+                id=""
+                rows="3"
+                className="border-2 border-black"
+              ></textarea>
             </div>
-             <div className="">
+            <div className="">
               <button className="bg-red-200 py-2 px-5 rounded-md">
-                {
-                  formData.index == "" ? "Submit" : "Update"
-                }
+                {formData.index == "" ? "Submit" : "Update"}
               </button>
-             </div>
+            </div>
           </form>
+        </div>
+
+        <div className="">
+          <table className="border-2">
+            <thead className="border-2">
+              <tr>
+                <th className="border-2">Id</th>
+                <th className="border-2">Email</th>
+                <th className="border-2">Name</th>
+                <th className="border-2">Phone</th>
+                <th className="border-2">Message</th>
+                <th className="border-2">Action</th>
+              </tr>
+            </thead>
+            {userData.length >= 1 ? (
+              <tbody>
+                {[...userData].reverse().map((data, i) => {
+                  return (
+                    <tr key={i} className="border-2">
+                      <td className="border-2">{i + 1}</td>
+                      <td className="border-2">{data.uname}</td>
+                      <td className="border-2">{data.uemail}</td>
+                      <td className="border-2">{data.uphone}</td>
+                      <td className="border-2">{data.umessage}</td>
+                      <td className="border-2">
+                        <button className="bg-red-200 px-4 py-1 mr-2">
+                          Delet
+                        </button>
+                        <button className="bg-blue-200 px-4 py-1">Edit</button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            ) : (
+              "data not found"
+            )}
+          </table>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
